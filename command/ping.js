459dfd9d8828175157e7f2afa040629887056ec2
Yalo <69597743+Yaloman@users.noreply.github.com>
2023-11-16 00:27:17 +0100

    Add files via upload

---
+++ command/ping.js
@@ -0,0 +1,18 @@
+const { Message, MessageEmbed} = require("discord.js")
+
+
+
+module.exports = {
+    name: "ping",
+    description: "This command utalizes the purpose for the bot to show the ping m/S",
+    access: {
+        req: 0,
+        owner: false
+    },
+
+    run: async(client, message, args) => {
+
+    }
+    
+
+}
